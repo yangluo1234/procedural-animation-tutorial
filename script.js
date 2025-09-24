88077c897dcde7e98b135472db0a245180559dcc
Yang L <s3354967@student.rmit.edu.au>
2025-09-24 15:20:21 +1000

    added

--- script.js
+++ script.js
@@ -9,12 +9,51 @@ let aBThree = document.getElementById("animButtonThree");
 aBThree.addEventListener("click", animateThree);
 
 function animateOne() {
-  //
+  //check current animation state
+  if (circle.style.animationPlayState === "paused") {
+    //we want to start animation playback when clicked
+    circle.style.animationPlayState = "running";
+  } else {
+    //we want to start animation playback when clicked
+    circle.style.animationPlayState = "paused";
+  }
 }
 
 /* https://developer.mozilla.org/en-US/docs/Web/API/Web_Animations_API/Using_the_Web_Animations_API */
 function animateTwo() {
-  //
+  //create animation from scratch
+  let animTiming = {
+    duration: 3000,
+    easing: "ease-in",
+    interactions: Infinity,
+  };
+  //create the keyframes
+  let animFrames = [
+    { transform: "translateX(10vh)", scale: 3 },
+    { transform: "translateX(-10vh)", scale: 2 },
+    { transform: "translateX(3vh)", scale: 1 },
+    { transform: "translateX(-6vh)", scale: 3 },
+  ];
+  circle.animate(animFrames, animTiming);
+}
+
+function animateThree() {
+  //generate random modifier
+  let randomMod = Math.random();
+  //create animation from scratch
+  let animTiming = {
+    duration: 3000 * randomMod,
+    easing: "ease-in",
+    interactions: Infinity,
+  };
+  //create the keyframes
+  let animFrames = [
+    { transform: `translateX(${10 * randomMod}vh)`, scale: 3 },
+    { transform: "translateX(-10vh)", scale: 2 },
+    { transform: "translateX(3vh)", scale: 1 },
+    { transform: "translateX(-6vh)", scale: 3 },
+  ];
+  circle.animate(animFrames, animTiming);
 }
 
 function animateFour() {
